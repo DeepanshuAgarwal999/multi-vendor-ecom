9a80482e93a12d693e3fe199356510640ba9c71a
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-06-08 22:09:11 +0530

    feat: add Apollo Client configuration and Providers component

---
+++ apps/user-ui/src/configs/apolloClient.ts
@@ -0,0 +1,30 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080/api/graphql',
+  credentials: 'include',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get token from localStorage if you're using token-based auth
+
+  return {
+    headers: {
+      ...headers,
+    },
+  };
+});
+
+export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+  },
+});
