ab8972adcc53a57fdd8d1fd1eacac37fc05f0c5f
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-06-08 22:08:12 +0530

    feat: add context function to GraphQL module for request handling

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloFederationDriver,
+      context: ({ req, res }) => ({ req, res }),
       playground: false,
       path: '/api/graphql',
       typePaths: ['./**/*.graphql'],
