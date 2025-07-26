5ad0f690120fc6ff4e9b1560fe8aaf1cafc3fd98
Deepanshu Agarwal <deepanshuagarwal9999@gmail.com>
2025-07-26 13:48:32 +0530

    feat: update user schema to include userType in ForgotPassword mutation and add registerSeller mutation

--- apps/seller-ui/src/app/(routes)/(auth)/signup/page.tsx
+++ apps/seller-ui/src/app/(routes)/(auth)/signup/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useMutation } from '@tanstack/react-query'
+import { Countries } from 'apps/seller-ui/src/utils/countries'
 import { UserService } from 'apps/user-ui/src/services/user.services'
 import GoogleButton from 'apps/user-ui/src/shared/components/google-button'
 import { Eye, EyeOff } from 'lucide-react'
@@ -13,7 +14,9 @@ import { toast } from 'sonner'
 type FormData = {
     email: string,
     password: string,
-    name: string
+    name: string,
+    phone_number: string
+    country: string
 }
 const SignUp = () => {
     const [activeStep, setActiveStep] = useState(1)
@@ -81,6 +84,7 @@ const SignUp = () => {
         },
         onError: (error) => {
             console.log(error);
+            setServerError(error.message)
             toast.error(error.message)
         }
     })
@@ -106,141 +110,157 @@ const SignUp = () => {
 
 
     return (
-        <div className='w-full flex flex-col items-center pt-10 min-h-screen bg-[#f1f1f1]' >
-            <div className="relative flex items-center justify-between md:w-1/2 mb-8">
-                <div className="absolute top-[25%] left-0 w-[80%] md:w-[90%] h-1 bg-gray-300 -z-10">
-                    {
-                        [1, 2, 3,].map((step, index) => (
-                            <div key={index}>
-                                <div className={`w-10 h-10 flex items-center justify-center rounded-full text-white font-bold ${step <= activeStep ? "bg-blue-600" : "bg-gray-300"}`} >
-                                    {step}
-                                </div>
-                                <span className='ml-[-15px]'>
-                                    {
-                                        step === 1 ? "Create Account" : step === 2 ? "Setup Shop" : "Connect Bank"
-                                    }
-                                </span>
+        <div className='w-full flex flex-col items-center pt-10 min-h-screen ' >
+            <div className="relative flex items-center justify-between md:w-[50%] mb-8">
+                <div className="absolute top-[25%] flex justify-between  left-0 w-[80%] md:w-[90%] h-1 bg-gray-300 -z-10" />
+                {
+                    [1, 2, 3,].map((step, index) => (
+                        <div key={index}>
+                            <div className={`w-10 h-10 flex items-center justify-center rounded-full text-white font-bold ${step <= activeStep ? "bg-blue-600" : "bg-gray-300"}`} >
+                                {step}
                             </div>
-                        ))
-                    }
-                </div>
-            </div>
-            <h1 className='text-4xl font-poppins font-semibold text-black text-center'>SignUp</h1>
-            <p className='text-center text-lg  font-medium py-3 text-[#00000099]'>
-                Home . SignUp
-            </p>
-            <div className='w-full flex justify-center'>
-                <div className='md:w-[480px] p-8  bg-white shadow rounded-lg '>
-                    <h3 className='text-3xl font-semibold text-center mb-2'>
-                        SignUp to AQuality
-                    </h3>
-                    <p className='text-center text-gray-500 mb-4 font-medium'>
-                        Already have an account?{" "}
-                        <Link href={'/login'} className='text-blue-500'>
-                            Login
-                        </Link>
-                    </p>
-                    <GoogleButton />
-                    <div className='flex items-center my-5 text-gray-400 text-sm'>
-                        <div className="flex-1 border-t border-gray-300"></div>
-                        <span className='px-3 text-gray-500 '>Or Sign in with Email</span>
-                        <div className="flex-1 border-t border-gray-300"></div>
-                    </div>
-                    {
-                        !showOtp ? <form action="" onSubmit={handleSubmit(onSubmit)}>
-                            <label htmlFor="name" className="block text-gray-700 mb-1 ">Name</label>
-                            <input type="text" placeholder='Your name' {...register('name', {
-                                required: 'Name is required',
-                                minLength: {
-                                    value: 4,
-                                    message: 'Name must be at least 4 characters'
-                                }
-                            })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
-
-                            {errors.name && <p className='text-red-500 text-xs italic'>{errors.name.message}</p>}
-                            <label htmlFor="email" className="block text-gray-700 mb-1 mt-4">Email</label>
-                            <input type="email" placeholder='support@aquality.io' {...register('email', {
-                                required: 'Email is required',
-                                pattern: {
-                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                    message: 'Invalid email address'
-                                }
-                            })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
-
-                            {errors.email && <p className='text-red-500 text-xs italic'>{errors.email.message}</p>}
-
-                            <label htmlFor='password' className="block text-gray-700 mb-1 mt-4 ">Password</label>
-                            <div className='relative'>
-                                <input type={passwordVisible ? 'text' : 'password'} placeholder='Your password' {...register('password', {
-                                    required: 'Password is required',
-                                    minLength: {
-                                        value: 6,
-                                        message: 'Password must be at least 6 characters'
-                                    }
-                                })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
-
-                                <button type='button' onClick={() => setPasswordVisible(!passwordVisible)} className='absolute inset-y-0 right-3  flex items-center text-gray-400'>
-                                    {passwordVisible ? <Eye size={18} /> : <EyeOff size={18} />}
-                                </button>
-                            </div>
-                            {errors.password && <p className='text-red-500 text-xs italic'>{errors.password.message}</p>}
-
-
-
-                            <button type='submit'
-                                disabled={signupMutation.isPending}
-                                className='w-full px-4 py-2 mt-4 text-white bg-black rounded-md hover:bg-black/80 '>
+                            <span className='ml-[-15px] text-black'>
                                 {
-                                    signupMutation.isPending ? "Signing up..." : "SignUp"
+                                    step === 1 ? "Create Account" : step === 2 ? "Setup Shop" : "Connect Bank"
                                 }
-                            </button>
-                            {
-                                serverError && <p className='text-red-500 text-xs italic mt-2'>{serverError}</p>
-                            }
-                        </form> : (
-                            <div>
-                                <h3 className='text-xl font-semibold text-center mb-4'>Enter Otp</h3>
-                                <div className='flex items-center justify-center gap-6'>
-                                    {
-                                        otp?.map((digit, index) => (
-                                            <input key={index} type='text' ref={
-                                                (el) => {
-                                                    if (el) {
-                                                        inputRefs.current[index] = el
-                                                    }
-                                                }}
-                                                maxLength={1}
-                                                className='w-12 h-12 text-center border border-gray outline-none rounded focus:ring-2 ring-blue-500'
-                                                value={digit}
-                                                onChange={(e) => handleOtpChange(e.target.value, index)}
-                                                onKeyDown={(e) => handleOtpKeyDown(e, index)}
-                                                onPaste={handleOnPaste}
-                                            />
-                                        ))
-                                    }
-                                </div>
-                                <button className='w-full py-2 rounded-lg mt-4 text-lg cursor-pointer bg-blue-500 text-white '
-                                    onClick={() => verifyOtpMutation.mutate()}
-                                    disabled={verifyOtpMutation.isPending}
-                                >
-                                    {verifyOtpMutation.isPending ? "Verifying..." : "verify OTP"}
-                                </button>
-                                <p className='text-center text-sm mt-4'>
-                                    {
-                                        canResend ? (
-                                            <button onClick={resendOtp} className='font-semibold text-blue-500 cursor-pointer'>
-                                                Resend OTP
-                                            </button>
-                                        ) : (
-                                            `Resend OTP in ${timer} seconds`
-                                        )
-                                    }
-                                </p>
-                            </div>
-                        )
-                    }
-                </div>
+                            </span>
+                        </div>
+                    ))
+                }
             </div>
+            <div className="md:w-[480px] p-8 bg-white shadow rounded-lg ">
+                {
+                    activeStep === 1 && (
+                        <>
+                            {
+                                !showOtp ? <form action="" onSubmit={handleSubmit(onSubmit)}>
+                                    <h3 className='text-2xl font-semibold text-center mb-4  '>
+                                        Create Account
+                                    </h3>
+                                    <label htmlFor="name" className="block text-gray-700 mb-1 ">Name</label>
+                                    <input type="text" placeholder='Your name' {...register('name', {
+                                        required: 'Name is required',
+                                        minLength: {
+                                            value: 4,
+                                            message: 'Name must be at least 4 characters'
+                                        }
+                                    })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
+
+                                    {errors.name && <p className='text-red-500 text-xs italic'>{errors.name.message}</p>}
+                                    <label htmlFor="email" className="block text-gray-700 mb-1 mt-4">Email</label>
+                                    <input type="email" placeholder='support@aquality.io' {...register('email', {
+                                        required: 'Email is required',
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: 'Invalid email address'
+                                        }
+                                    })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
+
+                                    {errors.email && <p className='text-red-500 text-xs italic'>{errors.email.message}</p>}
+                                    <label htmlFor="Phone" className='block text-gray-700 mb-1 mt-4'>Phone Number</label>
+                                    <input type='tel' placeholder='Your phone number' {...register('phone_number', {
+                                        required: 'Phone number is required',
+                                        pattern: {
+                                            value: /^[0-9]{10}$/,
+                                            message: 'Invalid phone number'
+                                        }
+                                    })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
+                                    {errors.phone_number && <p className='text-red-500 text-xs italic'>{errors.phone_number.message}</p>}
+
+
+                                    <label htmlFor='country' className="block text-gray-700 mb-1 mt-4 ">Country</label>
+                                    <select {...register('country', {
+                                        required: 'Country is required',
+                                    })} className='w-full px-3 py-2 max-h-64  border border-gray-300 rounded-md outline-none'>
+                                        <option value=''>Select your Country</option>
+                                        {
+                                            Countries.map((country) => (
+                                                <option key={country.code} value={country.code}>{country.name}</option>
+                                            ))
+                                        }
+                                    </select>
+
+                                    <label htmlFor='password' className="block text-gray-700 mb-1 mt-4 ">Password</label>
+                                    <div className='relative'>
+                                        <input type={passwordVisible ? 'text' : 'password'} placeholder='Your password' {...register('password', {
+                                            required: 'Password is required',
+                                            minLength: {
+                                                value: 6,
+                                                message: 'Password must be at least 6 characters'
+                                            }
+                                        })} className='w-full px-3 py-2 border border-gray-300 rounded-md outline-none' />
+
+                                        <button type='button' onClick={() => setPasswordVisible(!passwordVisible)} className='absolute inset-y-0 right-3  flex items-center text-gray-400'>
+                                            {passwordVisible ? <Eye size={18} /> : <EyeOff size={18} />}
+                                        </button>
+                                    </div>
+                                    {errors.password && <p className='text-red-500 text-xs italic'>{errors.password.message}</p>}
+
+
+                                    <button type='submit'
+                                        disabled={signupMutation.isPending}
+                                        className='w-full px-4 py-2 mt-4 text-white bg-black rounded-md hover:bg-black/80 '>
+                                        {
+                                            signupMutation.isPending ? "Signing up..." : "SignUp"
+                                        }
+                                    </button>
+                                    {
+                                        serverError && <p className='text-red-500 text-xs italic mt-2'>{serverError}</p>
+                                    }
+                                    <p className='pt-3 text-center'>
+                                        Already have an account?{" "}
+                                        <Link href={'/login'} className='text-blue-500'>
+                                            Login
+                                        </Link>
+                                    </p>
+                                </form> : (
+                                    <div>
+                                        <h3 className='text-xl font-semibold text-center mb-4'>Enter Otp</h3>
+                                        <div className='flex items-center justify-center gap-6'>
+                                            {
+                                                otp?.map((digit, index) => (
+                                                    <input key={index} type='text' ref={
+                                                        (el) => {
+                                                            if (el) {
+                                                                inputRefs.current[index] = el
+                                                            }
+                                                        }}
+                                                        maxLength={1}
+                                                        className='w-12 h-12 text-center border border-gray outline-none rounded focus:ring-2 ring-blue-500'
+                                                        value={digit}
+                                                        onChange={(e) => handleOtpChange(e.target.value, index)}
+                                                        onKeyDown={(e) => handleOtpKeyDown(e, index)}
+                                                        onPaste={handleOnPaste}
+                                                    />
+                                                ))
+                                            }
+                                        </div>
+                                        <button className='w-full py-2 rounded-lg mt-4 text-lg cursor-pointer bg-blue-500 text-white '
+                                            onClick={() => verifyOtpMutation.mutate()}
+                                            disabled={verifyOtpMutation.isPending}
+                                        >
+                                            {verifyOtpMutation.isPending ? "Verifying..." : "verify OTP"}
+                                        </button>
+                                        <p className='text-center text-sm mt-4'>
+                                            {
+                                                canResend ? (
+                                                    <button onClick={resendOtp} className='font-semibold text-blue-500 cursor-pointer'>
+                                                        Resend OTP
+                                                    </button>
+                                                ) : (
+                                                    `Resend OTP in ${timer} seconds`
+                                                )
+                                            }
+                                        </p>
+
+                                    </div>
+                                )
+                            }
+                        </>
+                    )
+                }
+            </div>
+
         </div >
     )
 }
